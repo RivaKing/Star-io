57ffccea1734ad007c5167f345d11573a29f803c
RivaKing <exgi2021@inbox.ru>
2025-03-31 14:18:13 +0300

    Использование web worker и изменение логики выстрела

--- client/main.js
+++ client/main.js
@@ -1,7 +1,8 @@
 import { resizeCanvas } from './utils.js';
-import { initPlayerControls, updatePlayer, isDead } from './player.js'; // Импортируем isDead
-import { draw } from './render.js';
+import { initPlayerControls, updatePlayer, isDead } from './player.js';
+import { draw, initBackground } from './render.js';
 import { initSocketListeners } from './network.js';
+import { processShoots } from './shoot.js';
 
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
@@ -18,12 +19,32 @@ export let player = { x: 100, y: 100, radius: 20, health: 100 };
 export let players = {};
 export let projectiles = [];
 
+const worker = new Worker('projectileWorker.js');
+
 initPlayerControls(canvas, socket);
 initSocketListeners(socket);
+initBackground();
 
-function gameLoop() {
-    updatePlayer();
+let lastTime = performance.now();
+
+worker.onmessage = (e) => {
+    const { updatedProjectiles, updatedPlayers } = e.data;
+    projectiles = updatedProjectiles;
+    for (let id in updatedPlayers) {
+        if (id !== socket.id) players[id] = updatedPlayers[id];
+    }
     draw(ctx);
+};
+
+function gameLoop(time) {
+    const deltaTime = (time - lastTime) / 1000;
+    lastTime = time;
+
+    updatePlayer();
+    processShoots();
+
+    worker.postMessage({ projectiles, players, deltaTime });
+    requestAnimationFrame(gameLoop);
 }
 
-setInterval(gameLoop, 1000 / 60);
+requestAnimationFrame(gameLoop);
